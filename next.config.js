3731ab14ec2533def9e907a5e167d4a464bb2c96
Noah Goldberg <noahgoldberg@Noahs-MacBook-Air-4.local>
2026-08-18 18:27:16 -0700

    Serve the Next landing page at the root

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return { beforeFiles: [{ source: "/", destination: "/site.html" }] };
-  },
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse"],
   },
